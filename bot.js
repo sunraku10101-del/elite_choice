d012b201be519c66af5eafdb06c7439c6e41bef3
sunraku10101-del <sunraku10101@gmail.com>
2025-12-23 13:48:49 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+<script>
 alert("BOT CONNECTED");
 
 const products = [
@@ -5,6 +6,18 @@ const products = [
     title: "Cetaphil Bright Healthy Radiance Serum",
     price: "₹347",
     image: "https://images.unsplash.com/photo-1585238342028-4bbcaa6f8a8b",
+    link: "#"
+  },
+  {
+    title: "L'Oreal Paris Revitalift Cream",
+    price: "₹499",
+    image: "https://images.unsplash.com/photo-1594759951986-d6d80b3d47ff",
+    link: "#"
+  },
+  {
+    title: "Nivea Face Wash",
+    price: "₹199",
+    image: "https://images.unsplash.com/photo-1600185362472-67d73e2f4d44",
     link: "#"
   }
 ];
@@ -18,7 +31,8 @@ products.forEach(p => {
     <img src="${p.image}">
     <h3>${p.title}</h3>
     <p>${p.price}</p>
-    <a href="${p.link}" class="buy">Buy Now</a>
+    <a href="${p.link}" class="buy" target="_blank">Buy Now</a>
   `;
   grid.appendChild(card);
 });
+</script>
